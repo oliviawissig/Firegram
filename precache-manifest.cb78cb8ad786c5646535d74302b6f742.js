ce8c5d6da694b738ea1cb2866b4601399286f824
Olivia Wissig <oliviawissig@gmail.com>
2021-12-22 12:37:57 -0800

    Updates

---
+++ precache-manifest.cb78cb8ad786c5646535d74302b6f742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd73b98a5aed976ee35ffd754ac1669f",
+    "url": "/Firegram/index.html"
+  },
+  {
+    "revision": "a25952ca7cb566e73b7a",
+    "url": "/Firegram/static/css/main.57235323.chunk.css"
+  },
+  {
+    "revision": "ec1c3be4b2db9a65879e",
+    "url": "/Firegram/static/js/2.3e081e63.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/Firegram/static/js/2.3e081e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a25952ca7cb566e73b7a",
+    "url": "/Firegram/static/js/main.c3c37724.chunk.js"
+  },
+  {
+    "revision": "51ac040677bfac14acc5",
+    "url": "/Firegram/static/js/runtime-main.c7c1e1fb.js"
+  }
+]);
